d90fffeb9e99a86c4bcc0f26a38b557f4d435068
Talha Tariq <talhacoder01@gmail.com>
2023-03-23 07:30:59 +0500

    Objects

---
+++ JS Solutions/Challange 1/script.js
@@ -0,0 +1,27 @@
+// Task 1
+console.log('Hello World')
+
+// Tqsk 2
+console.log('My Name is Talha Tariq')
+console.log('I am Learning JavaScript via Full Stack Bootcamp of DiaaTech')
+console.log('I will become the best developer by hard work.')
+console.log('I am making commitment to do code with consistency.')
+
+// vaiables | T1
+let name = 'Talha'
+let age = 22
+console.log(name, ' has a age of ', age)
+
+// BMI Question
+let markMass = 60
+let markHeight = 5.9
+
+let johnMass = 80
+let johnHeight = 5.8
+
+let markBMI = markMass / markHeight ** 2
+let johnBMI = johnMass / johnHeight ** 2
+
+console.log('John: ', johnBMI)
+
+console.log('Mark: ', markBMI)
